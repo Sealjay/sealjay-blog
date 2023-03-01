5159f39596e86cee8ccf53bdad7be34f9fc1c829
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2023-03-01 23:10:36 +0000

    Add speaking slots

--- astrosrc/src/content/config.ts
+++ astrosrc/src/content/config.ts
@@ -19,4 +19,23 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+
+const speaking = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		eventType: z.enum(["Conference", "Video", "Media Mention", "Podcast", "Workshop"]),
+		description: z.string(),
+		event: z.string(),
+		// Transform string to Date object
+		date: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		cta: z.string(),
+		url: z.string().url(),
+		thumbnail: z.string()
+	}),
+});
+
+export const collections = { blog, speaking };
