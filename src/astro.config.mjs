b619141bd4f7c8f29cde6344ec4b80ac65cb43d0
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2025-03-31 15:21:41 -0700

    Upgrades astro to v5

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,29 +1,13 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
 import react from '@astrojs/react'
 
-// https://astro.build/config
-import purgecss from 'astro-purgecss'
-
-// https://astro.build/config
-import compress from 'astro-compress'
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sealjay.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    react(),
-    purgecss({
-      safelist: ['aspect-[9/10]']
-    }),
-    compress()
-  ]
+  compressHTML: true,
+  integrations: [sitemap(), tailwind(), react(), mdx()]
 })
