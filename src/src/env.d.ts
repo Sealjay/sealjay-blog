47baa32cd5dec62bddfdea23ad578427420873e2
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2026-02-16 01:45:25 +0000

    feat: integrate Plausible analytics for tracking user interactions across various components

--- src/src/env.d.ts
+++ src/src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  plausible: ((event: string, options?: { props: Record<string, string> }) => void) | undefined
+}
